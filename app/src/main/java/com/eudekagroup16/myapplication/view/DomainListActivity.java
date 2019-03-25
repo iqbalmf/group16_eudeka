ae726d876039ef68afac43c4e1aefd9b70ab3d1a
adriyo <adriyoutomo@gmail.com>
2019-03-25 21:41:59 +0700

    menambahkan class viewmodel, fungsi untuk menampilkan data dari repository

--- app/src/main/java/com/eudekagroup16/myapplication/view/DomainListActivity.java
+++ app/src/main/java/com/eudekagroup16/myapplication/view/DomainListActivity.java
@@ -1,5 +1,6 @@
 package com.eudekagroup16.myapplication.view;
 
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -8,19 +9,25 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.MenuItem;
 
+import com.eudekagroup16.myapplication.Injection;
 import com.eudekagroup16.myapplication.R;
 import com.eudekagroup16.myapplication.adapter.DomainAdapter;
+import com.eudekagroup16.myapplication.databinding.ActivityDomainListBinding;
 import com.eudekagroup16.myapplication.listener.ItemDomainClickListener;
 import com.eudekagroup16.myapplication.model.DomainDetail;
+import com.eudekagroup16.myapplication.navigator.DomainListNavigator;
+import com.eudekagroup16.myapplication.viewmodel.DomainListViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class DomainListActivity extends AppCompatActivity implements ItemDomainClickListener, SwipeRefreshLayout.OnRefreshListener {
+public class DomainListActivity extends AppCompatActivity implements ItemDomainClickListener, DomainListNavigator {
 
     DomainAdapter adapter;
     RecyclerView domainRv;
     SwipeRefreshLayout refreshLayout;
+    DomainListViewModel viewModel;
+    ActivityDomainListBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,8 +47,13 @@ public class DomainListActivity extends AppCompatActivity implements ItemDomainC
     }
 
     @Override
-    public void onRefresh() {
-        refreshLayout.setRefreshing(false);
+    public void setLoading(boolean isLoading) {
+        refreshLayout.setRefreshing(isLoading);
+    }
+
+    @Override
+    public void loadDomainList(List<DomainDetail> domains) {
+        adapter.setItems(domains);
     }
 
     @Override
@@ -50,9 +62,11 @@ public class DomainListActivity extends AppCompatActivity implements ItemDomainC
     }
 
     private void initUi() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_domain_list);
+        viewModel = new DomainListViewModel(Injection.provideDomainRepository(this), this, this);
+        binding.setVm(viewModel);
         domainRv = findViewById(R.id.listDomain);
         refreshLayout = findViewById(R.id.refreshLayout);
-        refreshLayout.setOnRefreshListener(this);
         refreshLayout.setColorSchemeColors(ContextCompat.getColor(this, R.color.colorPrimary));
 
         if (getSupportActionBar() != null) {
@@ -65,19 +79,5 @@ public class DomainListActivity extends AppCompatActivity implements ItemDomainC
         adapter = new DomainAdapter(this);
         domainRv.setAdapter(adapter);
         domainRv.setLayoutManager(new LinearLayoutManager(this));
-        adapter.setItems(dummyData());
-    }
-
-    public List<DomainDetail> dummyData() {
-        List<DomainDetail> dummyList = new ArrayList<>();
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        dummyList.add(new DomainDetail("facebook.com", "SG"));
-        return dummyList;
     }
 }
