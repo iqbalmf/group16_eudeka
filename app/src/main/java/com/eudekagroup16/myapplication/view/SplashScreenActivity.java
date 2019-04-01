8de0b1551b6a5523969061d385cca6cee400849f
Iqbal MF <iqbalmf68@gmail.com>
2019-04-01 07:48:51 +0700

    implement constraint, menyamakan code

--- app/src/main/java/com/eudekagroup16/myapplication/view/SplashScreenActivity.java
+++ app/src/main/java/com/eudekagroup16/myapplication/view/SplashScreenActivity.java
@@ -1,8 +1,7 @@
 package com.eudekagroup16.myapplication.view;
 
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.eudekagroup16.myapplication.R;
